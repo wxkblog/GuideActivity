59802e5ae52e4525bf703131555cd01cb86c5649
wangxiaoke <wabi87547568@foxmail.com>
2016-04-10 21:23:37 +0800

    ViewFlipper

--- app/src/main/java/com/wang/administrator/guideactivity/MainActivity.java
+++ app/src/main/java/com/wang/administrator/guideactivity/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity implements android.view.GestureDetect
 
     private Activity mActivity = null;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
